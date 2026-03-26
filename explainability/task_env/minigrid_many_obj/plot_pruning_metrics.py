16fe52ecadcd4a8385da4059fbb8776682ef7a55
Yi-Ling Liu <yi-ling.liu@dfki.de>
2026-03-26 15:05:51 +0100

    Add plot of pruning metrics

---
+++ explainability/task_env/minigrid_many_obj/plot_pruning_metrics.py
@@ -0,0 +1,32 @@
+from aim import Repo
+import matplotlib.pyplot as plt
+
+repo = Repo(".")  # path to your Aim repo
+run_hash = ["bed5d8d8f2054ac9a45ae783","4622f6a875d74ad9a3f8574d","0e02dd4be3684817b54736af","951c1146502c47fb8df4e8be"]
+
+metrics = repo.query_metrics(
+    f"metric.name == 'hard sparsity' and run.hash in {run_hash}"
+)
+
+for metric in metrics:
+    print("metric from run:", metric.run.name)
+
+    data = list(metric.data)  # your raw structure
+
+    steps = []
+    values = []
+
+    for step, (value, epoch, timestamp) in data:
+        steps.append(step)
+        values.append(value*100)
+
+    experiment = metric.run.experiment
+    subtask = experiment.split("_")[1]
+
+    plt.plot(steps, values, label=f"task {subtask}")
+
+plt.xlabel("Step")
+plt.ylabel("Remaining weights (%)")
+plt.title("Subnetwork Weights Retained During Pruning")
+plt.legend()
+plt.show()
