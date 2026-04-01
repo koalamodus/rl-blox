4d3691058cb745acdcd53c497469851e21813f10
Yi-Ling Liu <yi-ling.liu@dfki.de>
2026-04-01 16:03:51 +0200

    Add holoocean prunning metrics plot

---
+++ explainability/task_env/holoocean/plot_pruning_metrics.py
@@ -0,0 +1,56 @@
+from aim import Repo
+import matplotlib.pyplot as plt
+
+file_name = "holoocean_subnetwork_pruning_weights.pdf"
+save_fig = True
+font_size = 16
+label_pad = 1
+title_pad = 25
+
+subnet_colors = {
+    "red": "#FF6666",      # moderately light red
+    "blue": "#66B2FF",     # moderately light blue
+    "green": "#66CC99",    # moderately light green
+    "black": "#999999"     # moderately light grey
+}
+
+repo = Repo("./../../../")  # path to your Aim repo
+run_hash = ["f17adbf89b5c4b38b0300ef5","2128dec00c754e78a051235d","12060991e7194ee283ebedeb","c14d16da1315495b8f10f87e"]  # desktop
+
+metrics = repo.query_metrics(
+    f"metric.name == 'hard sparsity' and run.hash in {run_hash}"
+)
+
+for metric in metrics:
+    print("metric from run:", metric.run.name)
+
+    data = list(metric.data)  # your raw structure
+
+    steps = []
+    values = []
+
+    for step, (value, epoch, timestamp) in data:
+        steps.append(step)
+        values.append(value*100)
+
+    experiment = metric.run.experiment
+    subtask = experiment.split("_")[1]
+
+    plt.plot(steps, values, color=subnet_colors[subtask], label=f"task {subtask}")
+
+plt.xlabel("step", fontsize=font_size)
+plt.ylabel("remaining weights (%)", fontsize=font_size, labelpad=label_pad)
+plt.title("Subnetwork Weights Retained During Pruning", fontsize=font_size, pad=title_pad)
+plt.legend(loc='lower right', fontsize=font_size)
+
+plt.tick_params(axis='x', labelsize=font_size)
+plt.tick_params(axis='y', labelsize=font_size)
+plt.xlim(left=0)
+plt.ylim(bottom=0)
+plt.tight_layout()
+
+if save_fig:
+    import os
+    path = os.path.expanduser(f'~/Pictures/{file_name}')
+    plt.savefig(path)
+plt.show()
